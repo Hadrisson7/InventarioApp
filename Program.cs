85fe5a1cb74bc29c25cec08bda09adbfec7b0d99
Hadrisson7 <wilney_2108@hotmail.com>
2026-05-03 13:07:34 -0500

    Primer push con estructura base del proyecto y configuracion de gitignore

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿
+using System.Reflection;
+
+var assembly = Assembly.GetExecutingAssembly();
+var version = assembly.GetName().Version;
+
+Console.WriteLine($"Version: {version}");
+Console.WriteLine($"Plataforma: {Environment.OSVersion}");
+Console.WriteLine($".Net Version: {Environment.Version}");
